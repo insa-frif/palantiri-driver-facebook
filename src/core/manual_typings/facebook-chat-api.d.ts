7d6b35736f8b815bc38ca94e54adc4ee5db40fc1
Charles "Demurgos" Samborski <demurgos.net@gmail.com>
2016-04-09 16:36:31 +0200

    Started typings for facebook-chat-api

--- src/core/manual_typings/facebook-chat-api.d.ts
+++ src/core/manual_typings/facebook-chat-api.d.ts
@@ -3,11 +3,32 @@
  */
 
 declare module "facebook-chat-api" {
-	interface api {
-
+	interface FacebookChatApi {
+    addUserToGroup(userID: number, threadID: number, callback?: (err: Error) => any): void;
 	}
 
-	interface err {
-		error: string;  // TODO : any ?
-	}
+  interface Credentials {
+    email: string;
+    password: string;
+  }
+
+  interface AppStateContainer {
+    appState: any;
+  }
+
+  interface LoginOptions {
+    // TODO
+  }
+
+  interface LoginError {
+    error: string;
+  }
+
+  interface staticFacebookChatApi { // login
+    (credentials: Credentials | AppStateContainer, callback?: (err: LoginError, api: FacebookChatApi) => any): void;
+    (credentials: Credentials | AppStateContainer, options: LoginOptions, callback?: (err: LoginError, api: FacebookChatApi) => any): void;
+  }
+
+  let facebookChatApi: staticFacebookChatApi;
+  export = facebookChatApi;
 }
---
+++ src/node/main.ts
@@ -0,0 +1,14 @@
+import * as login from "facebook-chat-api";
+
+login(
+  {email: "FB_EMAIL", password: "FB_PASSWORD"},
+  function callback (err, api) {
+    if(err){
+      return console.error(err.error);
+    }
+
+    // api.listen(function callback(err, message) {
+    //   api.sendMessage(message.body, message.threadID);
+    // });
+  }
+);
