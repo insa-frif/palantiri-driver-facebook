153b134dba572f40d1a8c3a1963525ddaa5d0ac7
Sn0wFox <ruben.pericas@laposte.net>
2016-04-17 19:36:12 +0200

    Added beginning of FacebookProxy.getOrCreateConnection()

--- src/node/facebook-driver.ts
+++ src/node/facebook-driver.ts
@@ -14,7 +14,9 @@ import {MSG_FLAG_EDI} from "../core/interfaces"
 import {Connection} from "../core/interfaces";
 import {OChatEmitter} from "../core/interfaces";
 import {Listener} from "../core/interfaces";
+import {OChatConnection} from "../core/OChat";
 import * as login from "facebook-chat-api";
+let readline = require('readline');
 
 class FacebookProxy implements Proxy {
 	protocol: string;
@@ -26,7 +28,48 @@ class FacebookProxy implements Proxy {
 	}
 
 	getOrCreateConnection(account: UserAccount): Promise<Connection> {
-		return undefined;
+		// TODO : the parameter is useless,
+		//        since that it's the userAccount that
+		//        call this methods, and that the userAccount
+		//        has this proxy as attribute.
+		if(this.connection) {
+			if(this.connection.connected) {
+				return Promise.resolve(this.connection);
+			}
+		}
+
+		let connection: Connection = new OChatConnection();
+		connection.connected = false;
+		connection.emitter = new OChatEmitter();
+		connection.listeners = [];
+
+		this.connection = connection;
+
+		let rl = readline.createInterface({
+			input: process.stdin,
+			output: process.stdout
+		});
+
+		// TODO : once the database will be fonctionnal, get IDs from it
+		rl.question("email : ", function (mail: string) {
+			let username = mail;
+			rl.question("pass : ", function (passw: string) {
+				let password = passw;
+				login(
+					{email: username, password: password},
+					function callback (err, api) {
+						if(!err) {
+							this.connection.connected = true;
+							// TODO : here we need to pass the api object to someone,
+							//        so we will be able to do the stuffs we want later
+						} else {
+							return;
+						}
+					}
+				);
+			});
+		});
+		return Promise.resolve(connection);
 	}
 
 	getContacts(account: UserAccount): Promise<Contact[]> {
