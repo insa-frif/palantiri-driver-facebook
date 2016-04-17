b9b17a26f360d8342fa34571bbceff48320de1bf
Sn0wFox <ruben.pericas@laposte.net>
2016-04-17 19:11:34 +0200

    Updated class FacebookProxy

--- src/node/facebook-driver.ts
+++ src/node/facebook-driver.ts
@@ -2,23 +2,41 @@
  * Created by Ruben on 11/04/2016.
  */
 
-import * as ochat from '../core/interfaces';
+import {Client} from "../core/interfaces";
+import {Proxy} from "../core/interfaces";
+import {User} from "../core/interfaces";
+import {Contact} from "../core/interfaces";
+import {Discussion} from "../core/interfaces";
+import {UserAccount} from "../core/interfaces";
+import {ContactAccount} from "../core/interfaces";
+import {Message} from "../core/interfaces";
+import {MSG_FLAG_EDI} from "../core/interfaces"
+import {Connection} from "../core/interfaces";
+import {OChatEmitter} from "../core/interfaces";
+import {Listener} from "../core/interfaces";
 import * as login from "facebook-chat-api";
 
-class FacebookProxy implements ochat.Proxy {
+class FacebookProxy implements Proxy {
+	protocol: string;
+
+	connection: Connection;
 
 	isCompatibleWith(protocol: string): boolean {
-		return protocol === "facebook";
-	}
-
-	getContacts(account: ochat.Account): ochat.Contact[] {
-		// TODO
 		return undefined;
 	}
 
-	sendMessage(msg:ochat.Message, discussion: ochat.Discussion, target: ochat.Contact): any {
-		// TODO
+	getOrCreateConnection(account: UserAccount): Promise<Connection> {
 		return undefined;
 	}
 
+	getContacts(account: UserAccount): Promise<Contact[]> {
+		return undefined;
+	}
+
+	getDiscussions(account: UserAccount, max?: number, filter?: (discuss: Discussion) => boolean): Promise<Discussion[]> {
+		return undefined;
+	}
+
+	sendMessage(msg: Message, recipient: ContactAccount, callback?: (err: Error, succesM: Message) => any): void {
+	}
 }
