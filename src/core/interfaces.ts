e96815938898a2b3ea170235caf956162bee57ee
Sn0wFox <ruben.pericas@laposte.net>
2016-04-18 19:48:12 +0200

    Added interface GroupAccount

--- src/core/interfaces.ts
+++ src/core/interfaces.ts
@@ -434,6 +434,33 @@ export interface ContactAccount {
 }
 
 /***************************************************************
+ * GroupAccount represents an aggregation of several
+ * ContactAccounts. This allows us to send a message to an
+ * existant discussion group instead of sending it to each
+ * member separatly, losing the idea of "group" in the
+ * contact's side.
+ * Note that the field "protocol" of each ContactAccount in
+ * members must be the same that the field "protocol" of
+ * this object, to avoid errors later.
+ ***************************************************************/
+export interface GroupAccount {
+	protocol: string;           //  Le protocole associe a ces comptes.
+
+	members: ContactAccount[];  //  La liste de tous les membres du
+															//  groupe de discussion.
+
+	addMembers(members: ContactAccount | ContactAccount[], callback?: (err: Error, members: ContactAccount[]) => any): void;
+	//  Add all the ContactAccounts "members" to the list of
+	//  known members.
+	//  Note that the ContactAccount with the field "protocol"
+	//  different of the field "protocol" of the current object
+	//  will not be added. The same applies to accounts already
+	//  existing in members.
+	//  If at least one account can not be added, err will not
+	//  be null.
+}
+
+/***************************************************************
  * OChatEmitter is the object that send Events to Connections.
  * You can handle any sort of Events by using .on().
  * You can send any Event by using .emit(). Events for which
