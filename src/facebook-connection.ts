4609adf6b8ce4b0193476d01b149ff7db6feabc1
Sn0wFox <ruben.pericas@laposte.net>
2016-04-21 12:42:34 +0200

    Added FacebookConnection class

---
+++ src/facebook-connection.ts
@@ -0,0 +1,27 @@
+/**
+ * Created by Ruben on 21/04/2016.
+ */
+import {EventEmitter} from "events";
+import {FacebookConnectedApi} from "./facebook-connected-api";
+import {Connection} from "palantiri-interfaces";
+import {UserAccount} from "palantiri-interfaces";
+import * as Bluebird from "bluebird";
+
+export class FacebookConnection extends EventEmitter implements Connection {
+  connected: boolean;
+
+  connectedApi: FacebookConnectedApi;
+
+  connect(userAccount: UserAccount):Bluebird.Thenable<FacebookConnectedApi> {
+    return undefined;
+  }
+
+  disconnect(): Bluebird.Thenable<Connection> {
+    return undefined;
+  }
+
+  getConnectedApi(): Bluebird.Thenable<FacebookConnectedApi> {
+    return undefined;
+  }
+
+}
